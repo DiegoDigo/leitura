03498874733e547943121e49cbf9aab68b937a98
diego.delmiro <di3g0d0ming05@gmail.com>
2017-03-27 16:48:31 -0300

    Ajuste no programa

--- leitura.py
+++ leitura.py
@@ -1,5 +1,9 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
+import fileinput
+import re
+from more_itertools import unique_everseen
+import sys
 
 print("Escutando alteraçoes")
 print("*------------------*")
@@ -9,36 +13,62 @@ caminho = ""
 nome_parte1 = nome_arquivo[0:2]
 nome_parte2 = nome_arquivo[-1]
 
-# if nome_parte2 == 'A' or nome_parte2 == 'E' or nome_parte2 == 'M' or nome_parte2 == 'P' or nome_parte2 == 'I':
-#     caminho = "F:\PRGNEW\%s\FONTES\%s" % (nome_parte1, nome_arquivo+".CBL")
-#     print(caminho)
-
 caminho = nome_arquivo + ".CBL"
 
-
-def trocarREAD(texto,novoTexto):
-    arq.write(lines.replace(texto, novoTexto))
-
-# try:
-listaCOPY = []
-arq = open(caminho, 'r+')
-# arquivo_novo = open(caminho, 'a+')
-for lines in arq.readlines():
-    # if lines.count("COPY"):
-    #    # if lines[lines.find("COPY")+32:45] == 'EMI"':
-    #    #     print(lines[lines.find("COPY")+32:27])
-    #    #     print(lines)
-
-    if lines.count("READ"):
-        frase = lines[lines.find("READ"):lines.find("READ")+14]
-        if frase[-1] == ".":
-            trocarREAD(".", " WITH NO LOCK")
-
-       # arq.write(lines)
-
+arq = open(caminho, 'r')
+arquivo = arq.readlines()
 arq.close()
+copys = []
 
-# except:
-#     print("arquivo nao encontrado !")
+
+def replace(TextAntigo, TextNovo, line):
+    return re.sub(TextAntigo, TextNovo, line.rstrip())
+
+for lines in arquivo:
+    if lines[lines.find("COPY")+32:45] == 'EMI"':
+        copys.append(lines[lines.find("COPY")+23:40])
+
+myfile = fileinput.FileInput(caminho, inplace=1)
+
+for line in myfile:
+    for copy in list(unique_everseen(copys)):
+        if line.__contains__(r"READ %s." % copy) and not line.__contains__("WITH NO LOCK") and not line.__contains__("PREVIOUS") \
+                and not line.__contains__("AT END") and not line.__contains__("NEXT"):
+                line = re.sub(r"READ %s." % copy, r"READ %s WITH NO LOCK " % copy, line.rstrip())+ "\n"
+
+        elif line.__contains__(r"READ %s" % copy) and not line.__contains__("WITH NO LOCK") and not line.__contains__("PREVIOUS") \
+                and not line.__contains__("AT END") and not line.__contains__("NEXT"):
+                line = re.sub(r"READ %s" % copy, r"READ %s WITH NO LOCK " % copy, line.rstrip())+ "\n"
+
+        elif line.__contains__(r"READ %s AT END" % copy) and not line.__contains__("WITH NO LOCK") and not line.__contains__("PREVIOUS") \
+                and not line.__contains__("NEXT"):
+                line = re.sub(r"READ %s AT END " % copy, r"READ %s WITH NO LOCK AT END" % copy, line.rstrip())+ "\n"
+
+        elif line.__contains__(r"READ %s NEXT." % copy) and not line.__contains__("WITH NO LOCK") and not line.__contains__("PREVIOUS") \
+                and not line.__contains__("AT END"):
+                line = re.sub(r"READ %s NEXT." % copy, r"READ %s NEXT WITH NO LOCK " % copy, line.rstrip())+ "\n"
+
+        elif line.__contains__(r"READ %s PREVIOUS." % copy) and not line.__contains__("WITH NO LOCK") and not line.__contains__("NEXT") \
+                and not line.__contains__("AT END"):
+                line = re.sub(r"READ %s PREVIOUS." % copy, r"READ %s PREVIOUS WITH NO LOCK " % copy, line.rstrip())+ "\n"
+
+        elif line.__contains__(r"READ %s NEXT" % copy) and not line.__contains__("WITH NO LOCK") and not line.__contains__("PREVIOUS") \
+                and not line.__contains__("AT END"):
+                line = re.sub(r"READ %s NEXT " % copy, r"READ %s NEXT WITH NO LOCK " % copy, line.rstrip())+ "\n"
+
+        elif line.__contains__(r"READ %s PREVIOUS" % copy) and not line.__contains__("WITH NO LOCK") and not line.__contains__("NEXT") \
+                and not line.__contains__("AT END"):
+                line = re.sub(r"READ %s PREVIOUS " % copy, r"READ %s PREVIOUS WITH NO LOCK " % copy, line.rstrip()) + "\n"
+
+        elif line.__contains__(r"READ %s KEY IS" % copy) and not line.__contains__("WITH NO LOCK") and line.__contains__(r"NEXT" % copy):
+                line = re.sub(r"READ %s KEY IS " % copy, r"READ %s WITH NO LOCK '\n' KEY IS" % copy, line.rstrip()) + "\n"
+
+    sys.stdout.write(line)
+
+myfile.close()
+
+
+
+
 
 
